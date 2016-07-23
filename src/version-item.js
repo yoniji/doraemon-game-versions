f0e66abe423f262b53cf07a265ca38cff727baea
yoniji <hello.hyu@gmail.com>
2016-07-23 21:07:39 +0800

    fix link bug

--- src/version-item.js
+++ src/version-item.js
@@ -14,13 +14,13 @@ export default class VersionItem extends React.Component {
       <div className="versionName">{this.props.children}</div>
       <div className="createdAt">{createdAtStr}</div>
       <div className="download">
-        <a className="download-ios downloadBtn" href="{this.props.ios}">
+        <a className="download-ios downloadBtn" href={this.props.ios}>
           <i className="fa fa-apple" />
         </a>
-        <a className="download-android downloadBtn" href="{this.props.android}">
+        <a className="download-android downloadBtn" href={this.props.android}>
           <i className="fa fa-android" />
         </a>
-        <a className="download-server downloadBtn" href="{this.props.server}">
+        <a className="download-server downloadBtn" href={this.props.server}>
           <i className="fa fa-server" />
         </a>
       </div>
