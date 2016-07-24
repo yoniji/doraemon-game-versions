64095899f610dbcf6e7931d6cfe5f87a248c1590
yoniji <hello.hyu@gmail.com>
2016-07-24 15:16:46 +0800

    优化手机端显示，增加展开功能

--- src/filter-item.js
+++ src/filter-item.js
@@ -4,25 +4,55 @@ import VersionItem from './version-item';
 
 
 export default class FilterItem extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      expanded: false
+    };
+  }
+
   render() {
+    let className = this.state.expanded?'expanded':'';
+
+    const hiddenVersionsCount = this.props.versions.length - 2;
+    if (hiddenVersionsCount > 0) {
+      className += ' showMoreBtn';
+    }
+
+    let moreBtnText;
+    if (this.state.expanded) {
+      moreBtnText = `Hide Older Versions (${hiddenVersionsCount})`;
+    } else {
+      moreBtnText = `Show Older Versions (${hiddenVersionsCount})`;
+    }
+
+
+
     const versionNodes = this.props.versions.map((versionData) => {
+      const {id, versionName, ...others} = versionData;
+
       return <VersionItem
-        key={versionData.id}
-        ios={versionData.ios}
-        android={versionData.android}
-        server={versionData.server}
-        createdAt={versionData.createdAt}>
-        {versionData.versionName}
+        key={id}
+        {...others}>
+        {versionName}
       </VersionItem>;
     });
 
-    return <div className="filterItem">
+    return <div className={'filterItem ' + className}>
       <h2 className="filterName">
         {this.props.title}
       </h2>
       <ul className="versionList">
         {versionNodes}
       </ul>
+      <div className="filterList-more" onClick={this.handleClick.bind(this)}>
+        {moreBtnText}
+      </div>
     </div>;
+  }
+
+  handleClick() {
+    this.setState({expanded: !this.state.expanded});
   }
 };
